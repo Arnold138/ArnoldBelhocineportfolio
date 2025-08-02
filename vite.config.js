64ff9e622ce22e90f13beecd7e70b21fe37dae80
TonNom <tonemail@exemple.com>
2025-08-02 22:30:26 +0200

    Build propre pour GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: "/Portfolio-D-Arnold/",
-  plugins: [react()],
-})
+export default {
+  base: './', // ou '/nom-du-repo/' si c’est dans un sous-dossier
+  build: {
+    outDir: 'docs'
+  }
+}
