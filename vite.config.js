5266072d4d3f46d720b2229f09e8b56fe4e7c603
TonNom <tonemail@exemple.com>
2025-08-02 22:35:30 +0200

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+
 
 
 export default defineConfig({
-  plugins: [react()],
-  base: './',
+  // ...autres plugins...
+  base: '/ArnoldBelhocineportfolio/',
   build: {
     outDir: 'docs',
-    emptyOutDir: true, // Supprime l'ancien contenu avant chaque build
+    emptyOutDir: true
   }
 });
+
