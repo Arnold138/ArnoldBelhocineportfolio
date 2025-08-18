6498e0d76d91fecec39100787c33656ebd78e6c2
TonNom <tonemail@exemple.com>
2025-08-18 13:16:45 +0200

    go

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import './App.css'
 import NavBarr from './components/NavBarr'
 import HeroSection from './components/HeroSection'
 import ProjectsSection from './components/ProjectsSection'
